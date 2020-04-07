c1868a6618970cc6112e3625ced84ca487b68827
Fu10087 <568469277@qq.com>
2020-04-07 16:17:00 +0800

    Add files via upload

--- lib/flutter_page_enable.dart
+++ lib/flutter_page_enable.dart
@@ -1,7 +1,47 @@
 library flutter_page_enable;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class PageEnableScrollPhysics extends ScrollPhysics {
+
+  final double pageSize;
+
+  const PageEnableScrollPhysics({ ScrollPhysics parent, this.pageSize}) : super(parent: parent);
+
+  @override
+  PageEnableScrollPhysics applyTo(ScrollPhysics ancestor) {
+    return PageEnableScrollPhysics(parent: buildParent(ancestor), pageSize: this.pageSize);
+  }
+
+  double _getPage(ScrollMetrics position) {
+    return position.pixels / pageSize;
+  }
+
+  double _getPixels(ScrollMetrics position, double page) {
+    return page * pageSize;
+  }
+
+  double _getTargetPixels(ScrollMetrics position, Tolerance tolerance, double velocity) {
+    double page = _getPage(position);
+    if (velocity < -tolerance.velocity)
+      page -= 0.5;
+    else if (velocity > tolerance.velocity)
+      page += 0.5;
+    return _getPixels(position, page.roundToDouble());
+  }
+
+  @override
+  Simulation createBallisticSimulation(ScrollMetrics position, double velocity) {
+    if ((position.pixels <= position.minScrollExtent) ||
+        (position.pixels >= position.maxScrollExtent))
+      return super.createBallisticSimulation(position, velocity);
+    final Tolerance tolerance = this.tolerance;
+    final double target = _getTargetPixels(position, tolerance, velocity);
+    if (target != position.pixels)
+      return ScrollSpringSimulation(spring, position.pixels, target, velocity, tolerance: tolerance);
+    return null;
+  }
+
+  @override
+  bool get allowImplicitScrolling => false;
 }
